1c74d2b6f6b0a0f3eeaf9c75b1ebac6d3fb68d34
Gurey <gustav.holfve@gmail.com>
2014-02-26 16:06:32 +0100

    Animation

--- src/explosionBoy/server/Server.java
+++ src/explosionBoy/server/Server.java
@@ -91,7 +91,7 @@ public class Server implements Runnable {
 
 	public void send(ConnectionReference cr, InetAddress ip, int port) {
 		jsonToSend.setDirection(cr.getDir());
-		jsonToSend.setSpeed(4);
+		jsonToSend.setSpeed(3);
 		sendData = gson.toJson(jsonToSend, Json.class).getBytes();
 		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ip, port);
 		try {
