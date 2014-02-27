7cd6070dc7dd271d7e874ceabe55e2d19c5fa92e
Bambuuh <lukas@klingberg.se>
2014-02-27 12:44:13 +0100

    pID

--- src/explosionBoy/client/ServerConnection.java
+++ src/explosionBoy/client/ServerConnection.java
@@ -45,7 +45,6 @@ public class ServerConnection implements Runnable {
 	
 	public void send(Json json){
 		json.setgID(1);
-		json.setpID(1);
 		sendData = gson.toJson(json, Json.class).getBytes();
 		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ip, 9876);
 		try {
